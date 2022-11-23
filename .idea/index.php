f5d01001aa68a6ee125742e47cde8693a975fc61
gim-yong-won <tjaaj159@naver.com>
2022-11-23 17:45:03 +0900

    php mysql 공부

---
+++ .idea/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>WEB</title>
+</head>
+    <body>
+    <h1>WEB</h1>
+    <ol>
+        <li>HTML</li>
+    </ol>
+    <h2>WELCOME</h2>
+    Lorem It is a long established fact that a reader
+    will be distracted by
+    </body>
+</html>
